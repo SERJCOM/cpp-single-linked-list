9714d897fa195a7523fc816f3aa0c0598ca0a20d
SERJCOM <sergeylukin228@yandex.ru>
2023-04-03 11:49:27 +0300

    new changes:

---
+++ build/CMakeFiles/temp.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/temp.dir/main.cpp.o"
+  "CMakeFiles/temp.dir/main.cpp.o.d"
+  "temp"
+  "temp.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/temp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
